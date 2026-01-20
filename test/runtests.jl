f2be01e6a17fb2adf042484d25a0f4e193c9752f
myles-codes-elve <myles.holley@elvespeed.com>
2026-01-20 13:28:01 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -72,6 +72,35 @@ end
 
   # Test isoof function
   @test iso_of(C) == 12
+end
+
+
+@testset "Gyrovals Functions" begin
+  #Create test species
+  e = Species("electron")
+  C = Species("12C")
+
+  #Test g_spin function
+  g_spin(e) ≈ 2
+  g_spin(C) ≈ 2
+
+  #Test gyromagnetic_anomaly function
+  gyromagnetic_anomaly(e) ≈ 2
+  gyromagnetic_anomaly(C) ≈ 2
+
+  g_nucleon(C) ≈ 2
+end
+
+@testset "Helper Functions" begin
+  # Create test species
+  C = Species("12C+")
+  @test find_superscript(C) == '+' 
+
+  #Test normalize_superscipt Function
+  @test normalize_superscripts(C) == '+'
+
+  #Test chargeparse function
+  @test chargeparse(C) == 1
 
 end
 
